f191e5080901c22cdd9163f544e1fbed37d3700b
Restu Ardiansyah <ardiansyahrestu713@gmail.com>
2026-05-13 00:12:55 +0700

    atomic design page sign in dan sign up

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AuthLayout from "../components/Layouts/AuthLayout";
+import FromSignUp from "../components/Fragments/FromSignUp";
+
+const SignUp = () => {
+  return (
+    <AuthLayout>
+      <FromSignUp />
+    </AuthLayout>
+  );
+};
+
+export default SignUp;
