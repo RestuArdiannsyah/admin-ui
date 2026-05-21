3606f2e25ef7a0f9db5cfcec94dcb508edfde52b
Restu Ardiansyah <ardiansyahrestu713@gmail.com>
2026-05-21 16:00:11 +0700

    Responsive Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,15 @@ import { Link } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ErrorPage from "./pages/error";
+import Dashboard from "./pages/Dashboard";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
   const myRouter = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <div className="flex justify-center items-center min-h-screen">
-          <Link to="/login" className="p-2 m-5 bg-primary text-white">
-            Login
-          </Link>
-          |
-          <Link to="/register" className="p-2 m-5 bg-primary text-white">
-            Register
-          </Link>
-        </div>
-      ),
+      path: "/dashboard",
+      element: <Dashboard />,
       errorElement: <ErrorPage />,
     },
     {
