cb09c79adce506d3099f775507761ca8f4905026
Restu Ardiansyah <ardiansyahrestu713@gmail.com>
2026-06-02 14:27:03 +0700

    State Management with Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
+// AI Asli Ini
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ThemeContextProvider } from "./context/themeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </StrictMode>,
 );
+
+// latihan
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import Parent from "./latihan/Parent.jsx";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Parent />
+//   </React.StrictMode>,
+// );
+
+// context
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import Parent from "./latihan/Parent.jsx";
+// import { CounterContextProvider } from "./context/counterContext.jsx";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <CounterContextProvider>
+//       <Parent />
+//     </CounterContextProvider>
+//   </React.StrictMode>,
+// );
